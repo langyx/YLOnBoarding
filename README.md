2f57145a983af86bde09a655ac20f7b6ef91e4b8
Yannis Lang <jopolaz@hotmail.fr>
2021-03-10 13:51:02 +0100

    documentation

--- README.md
+++ README.md
@@ -2,4 +2,29 @@
 
 [![Build Status](https://travis-ci.org/langyx/YLOnBoarding.svg?branch=main)](https://travis-ci.org/langyx/YLOnBoarding)
 
-Documentation coming ... 
+![This is a alt text.](/images/SS1.png "OnBoarding Screenshot")
+
+## Installation
+
+iOS 13+
+
+In Xcode go to `File -> Swift Packages -> Add Package Dependency` and paste in the repo's url: `https://github.com/langyx/YLOnBoarding` and select master branch.
+
+## Usage
+
+### Item :
+
+```Swift
+YLOnBoardingItem(icon: "bag.fill.badge.plus", title: "New content", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+```
+
+### Style :
+
+```Swift
+YLOnBoardingStyle(mainColor: .black, accentColor: .blue)
+```
+
+### OnBoarding
+```Swift
+YLOnBoarding(title: "Discover new features", style: YLOnBoardingStyle(), items: items)
+```
--- Sources/YLOnBoarding/YLOnBoarding.swift
+++ Sources/YLOnBoarding/YLOnBoarding.swift
@@ -45,10 +45,10 @@ public struct YLOnBoarding: View {
 struct YLOnBoarding_Previews: PreviewProvider {
     static var previews: some View {
         let items = [
-            YLOnBoardingItem(icon: "bag.fill.badge.plus", title: "New content", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."),
-            YLOnBoardingItem(icon: "bag.fill.badge.plus", title: "New content", description: "Discover new content in this update"),
-            YLOnBoardingItem(icon: "bag.fill.badge.plus", title: "New content", description: "Discover new content in this update")
+            YLOnBoardingItem(icon: "bag.fill.badge.plus", title: "New content", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+            YLOnBoardingItem(icon: "bolt.fill", title: "New content", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+            YLOnBoardingItem(icon: "heart.fill", title: "New content", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
         ]
-        return YLOnBoarding(title: "Discover new features", style: YLOnBoardingStyle(), items: items)
+        return YLOnBoarding(title: "Discover new features", style: YLOnBoardingStyle(mainColor: .black, accentColor: .blue), items: items)
     }
 }
